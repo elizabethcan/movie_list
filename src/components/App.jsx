97c927e47993b0e8801b7d007e32454af4291880
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-27 18:03:19 -0800

    Pass props into MovieRow component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
+function MovieRow(props) {
+	return (
+		<li>{props.movie}</li>
+	)
+}
 
 function App() {
 	return (
 		<div>
 			<h1>Movie List</h1>
+			<div>
+				<MovieRow movie={movies[0].title}/>
+				<MovieRow movie={movies[1].title}/>
+				<MovieRow movie={movies[2].title}/>
+				<MovieRow movie={movies[3].title}/>
+				<MovieRow movie={movies[4].title}/>
+			</div>
 		</div>
 	);
 }
