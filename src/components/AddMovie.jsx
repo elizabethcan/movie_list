e029d439ceeb5044b7dc73a30593479e345c1a78
Elizabeth Cannon <elizabeth230@gmail.com>
2019-05-10 14:45:16 -0700

    refactor addMovie to be stateless

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 
-class AddMovie extends React.Component {
-  constructor(props) {
-    super(props);
-    this.changeHandler = this.changeHandler.bind(this);
-    this.clickHandler = this.clickHandler.bind(this);
-  }
-
-  changeHandler(event) {
-    this.props.enterMovie(event)
-  }
-
-  clickHandler(event) {
-    event.preventDefault();
-    this.props.addToList();
-  }
-
-  render() {
-    console.log(`this.props: `, this.props);
-    return (
-      <div className="add-movie">
-        <input placeholder="Add Movie" onChange={(e) => this.changeHandler(e)}></input>
-        <button onClick={this.clickHandler}>Add</button>
-      </div>
-    );
-  }
+function AddMovie(props) {
+  return (
+    <div className="add-movie">
+      <input placeholder="Add Movie" onChange={props.enterMovie}></input>
+      <button onClick={props.addToList}>Add</button>
+    </div>
+  );
 }
 
 export default AddMovie;
